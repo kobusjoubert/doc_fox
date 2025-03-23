9cc8ae388708218c9abd8c1b052011c00ebc8e71
Kobus Joubert <kobus.joubert@platform45.com>
2025-03-23 11:08:02 +0200

    Setup Zeitwerk

--- lib/doc_fox.rb
+++ lib/doc_fox.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+require 'zeitwerk'
+
+loader = Zeitwerk::Loader.for_gem
+loader.setup
+
 require_relative 'doc_fox/version'
 
 module DocFox
   class Error < StandardError; end
-  # Your code goes here...
 end
